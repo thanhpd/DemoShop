9cf9f444f6108c9d1898407ddd7ef3a027af22b4
Phan Duy Thành <thanhpd.mac@gmail.com>
2015-08-26 13:50:31 +0700

    Add entity framework, created models

---
+++ ShoppingTech/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ShoppingTech.Models
+{
+    public class Category
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid CategoryId { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ ShoppingTech/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Security.Permissions;
+using System.Web;
+
+namespace ShoppingTech.Models
+{
+    public class Product
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ProductId { get; set; }
+
+        public string ProductCode { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal ListPrice { get; set; }
+
+        public decimal DiscountPercent { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public Category Category { get; set; }
+    }
+}
