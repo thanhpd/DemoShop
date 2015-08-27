4bc3fec5ad5b2aa34be0f9fe92d735ce035c8523
Phan Duy Thành <thanhpd.mac@gmail.com>
2015-08-27 14:58:30 +0700

    delete cat

--- ShoppingTech/Scripts/management.js
+++ ShoppingTech/Scripts/management.js
@@ -26,8 +26,35 @@ $("#addModal").submit(function (event) {
         error: function (jqXHR, textStatus, errorThrown) {
             alert(textStatus);
         }
-
     });
-
     return false;
 });
+
+$('.delete-category').on('click', function(e) {
+    var rowId = $(this).closest('tr').attr('id');
+    console.log("rowId" + rowId);
+    var id = $('#rowId');
+    id.innerHTML = rowId;
+    
+    $('#deleteModal').on('show.bs.modal', function() {        
+        $('#deleteModal').find('#rowId').html(rowId);
+    });
+});
+
+$('#delete-cat').on('click', function (e) {
+    e.preventDefault();
+    var rowId = $('#rowId')[0].innerText;
+    console.log(rowId);      
+   
+    $.ajax({
+        url: '../../api/Categories/' + rowId,
+        type: 'DELETE',        
+        async: true,        
+        success: function (data, textStatus, jqXHR) {
+            window.location.reload();
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            alert(textStatus);
+        }
+    });    
+})
